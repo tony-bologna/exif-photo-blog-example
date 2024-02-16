69d404920d5265e0d238af9013330ce05e40340d
Sam Becker <sam@sambecker.com>
2024-02-16 17:45:59 -0600

    Add conditional admin menu to photo tiles

--- src/photo/PhotoGrid.tsx
+++ src/photo/PhotoGrid.tsx
@@ -3,6 +3,7 @@ import PhotoSmall from './PhotoSmall';
 import { clsx } from 'clsx/lite';
 import AnimateItems from '@/components/AnimateItems';
 import { Camera } from '@/camera';
+import MorePhotos from '@/photo/MorePhotos';
 import { FilmSimulation } from '@/simulation';
 import { GRID_ASPECT_RATIO, HIGH_DENSITY_GRID } from '@/site/config';
 
@@ -16,6 +17,7 @@ export default function PhotoGrid({
   animate = true,
   animateOnFirstLoadOnly,
   staggerOnFirstLoadOnly = true,
+  showMorePath,
   additionalTile,
   small,
 }: {
@@ -33,47 +35,51 @@ export default function PhotoGrid({
   small?: boolean
 }) {
   return (
-    <AnimateItems
-      className={clsx(
-        'grid gap-0.5 sm:gap-1',
-        small
-          ? 'grid-cols-3 xs:grid-cols-6'
-          : HIGH_DENSITY_GRID
-            ? 'grid-cols-2 xs:grid-cols-4 lg:grid-cols-5'
-            : 'grid-cols-2 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4',
-        'items-center',
-      )}
-      type={animate === false ? 'none' : undefined}
-      duration={fast ? 0.3 : undefined}
-      staggerDelay={0.075}
-      distanceOffset={40}
-      animateOnFirstLoadOnly={animateOnFirstLoadOnly}
-      staggerOnFirstLoadOnly={staggerOnFirstLoadOnly}
-      items={photos.map(photo =>
-        <div
-          key={photo.id}
-          className={GRID_ASPECT_RATIO !== 0
-            ? clsx(
-              'aspect-square',
-              'overflow-hidden',
-              '[&>*]:flex [&>*]:w-full [&>*]:h-full',
-              '[&>*>*]:object-cover [&>*>*]:min-h-full',
-            )
-            : undefined}
-          style={{
-            ...GRID_ASPECT_RATIO !== 0 && {
-              aspectRatio: GRID_ASPECT_RATIO,
-            },
-          }}
-        >
-          <PhotoSmall {...{
-            photo,
-            tag,
-            camera,
-            simulation,
-            selected: photo.id === selectedPhoto?.id,
-          }} />
-        </div>).concat(additionalTile ?? [])}
-    />
+    <div className="space-y-4">
+      <AnimateItems
+        className={clsx(
+          'grid gap-0.5 sm:gap-1',
+          small
+            ? 'grid-cols-3 xs:grid-cols-6'
+            : HIGH_DENSITY_GRID
+              ? 'grid-cols-2 xs:grid-cols-4 lg:grid-cols-5'
+              : 'grid-cols-2 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4',
+          'items-center',
+        )}
+        type={animate === false ? 'none' : undefined}
+        duration={fast ? 0.3 : undefined}
+        staggerDelay={0.075}
+        distanceOffset={40}
+        animateOnFirstLoadOnly={animateOnFirstLoadOnly}
+        staggerOnFirstLoadOnly={staggerOnFirstLoadOnly}
+        items={photos.map(photo =>
+          <div
+            key={photo.id}
+            className={GRID_ASPECT_RATIO !== 0
+              ? clsx(
+                'aspect-square',
+                'overflow-hidden',
+                '[&>*]:flex [&>*]:w-full [&>*]:h-full',
+                '[&>*>*]:object-cover',
+              )
+              : undefined}
+            style={{
+              ...GRID_ASPECT_RATIO !== 0 && {
+                aspectRatio: GRID_ASPECT_RATIO,
+              },
+            }}
+          >
+            <PhotoSmall {...{
+              photo,
+              tag,
+              camera,
+              simulation,
+              selected: photo.id === selectedPhoto?.id,
+            }} />
+          </div>).concat(additionalTile ?? [])}
+      />
+      {showMorePath &&
+        <MorePhotos path={showMorePath} />}
+    </div>
   );
 };
