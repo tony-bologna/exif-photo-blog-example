2f37703709edb069ae9add26e177577e0b49fc67
Sam Becker <sam@sambecker.com>
2024-02-21 18:01:34 -0600

    Final cmd-k adjustments

--- src/site/NavClient.tsx
+++ src/site/NavClient.tsx
@@ -6,15 +6,14 @@ import Link from 'next/link';
 import SiteGrid from '../components/SiteGrid';
 import { SITE_DOMAIN_OR_TITLE } from '@/site/config';
 import ViewSwitcher, { SwitcherSelection } from '@/site/ViewSwitcher';
-import { PATH_ROOT,
+import {
+  PATH_ROOT,
   isPathAdmin,
   isPathGrid,
   isPathProtected,
   isPathSignIn,
-  PATH_ABOUT,
 } from '@/site/paths';
 import AnimateItems from '../components/AnimateItems';
-import Image from 'next/image';
 
 export default function NavClient({
   showAdmin,
@@ -34,7 +33,6 @@ export default function NavClient({
     typeof linkOrAction === 'string'
       ? <Link href={linkOrAction}>{text}</Link>
       : <button onClick={linkOrAction}>{text}</button>;
-
 
   const switcherSelectionForPath = (): SwitcherSelection | undefined => {
     if (pathname === PATH_ROOT) {
@@ -61,24 +59,15 @@ export default function NavClient({
                 'w-full min-h-[4rem]',
                 'leading-none',
               )}>
-              <div className="hidden xs:block mr-4">
-                {renderLink(SITE_DOMAIN_OR_TITLE, PATH_ROOT)}
-              </div>
-
-              <div className="flex flex-grow items-center gap-4">
+              <div className="flex-grow">
                 <ViewSwitcher
                   currentSelection={switcherSelectionForPath()}
                   showAdmin={showAdmin}
                 />
               </div>
-
-              <div className='mr-2'>
-              <Link href={PATH_ABOUT}>
-                About
-              {/* <Image className='hover:bg-black transition-all duration-500' src={'/staticPics/milli_aboout2.png'} width={100} height={50} alt="about section link"/> */}
-              </Link>
+              <div className="hidden xs:block text-right text-balance">
+                {renderLink(SITE_DOMAIN_OR_TITLE, PATH_ROOT)}
               </div>
-
             </div>]
             : []}
         />
