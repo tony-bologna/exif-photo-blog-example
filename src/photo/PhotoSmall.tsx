c5028fba23ac0d4824d2a27a0363a43d251ea054
Sam Becker <sam@sambecker.com>
2024-02-23 12:06:02 -0600

    Fade placeholder blurs

--- src/photo/PhotoSmall.tsx
+++ src/photo/PhotoSmall.tsx
@@ -5,8 +5,6 @@ import { clsx } from 'clsx/lite';
 import { pathForPhoto } from '@/site/paths';
 import { Camera } from '@/camera';
 import { FilmSimulation } from '@/simulation';
-import AdminPhotoMenu from '@/admin/AdminPhotoMenu';
-import { Suspense } from 'react';
 
 export default function PhotoSmall({
   photo,
@@ -14,43 +12,29 @@ export default function PhotoSmall({
   camera,
   simulation,
   selected,
-  showAdminMenu,
-  priority,
 }: {
   photo: Photo
   tag?: string
   camera?: Camera
   simulation?: FilmSimulation
   selected?: boolean
-  showAdminMenu?: boolean
-  priority?: boolean
 }) {
   return (
     <Link
       href={pathForPhoto(photo, tag, camera, simulation)}
       className={clsx(
-        'relative group',
+        'group',
+        'flex relative w-full h-full',
         'active:brightness-75',
         selected && 'brightness-50',
       )}
     >
-      <Suspense>
-        {showAdminMenu &&
-          <AdminPhotoMenu
-            buttonClassName={clsx(
-              'absolute top-1 right-1 opacity-0',
-              'group-hover:opacity-100 group-focus:opacity-100',
-            )}
-            photo={photo}
-          />}
-      </Suspense>
       <ImageSmall
         src={photo.url}
         aspectRatio={photo.aspectRatio}
         blurData={photo.blurData}
         className="w-full"
         alt={titleForPhoto(photo)}
-        priority={priority}
       />
     </Link>
   );
