7b7f1e6f32e0b16a3fc28d023f87b75be319e5ed
Sam Becker <sam@sambecker.com>
2024-03-23 10:42:37 -0500

    Hide admin nav scrollbar on windows

--- src/admin/AdminNav.tsx
+++ src/admin/AdminNav.tsx
@@ -1,55 +1,66 @@
-import { getStorageUploadUrlsNoStore } from '@/services/storage/cache';
+'use client';
+
+import SiteGrid from '@/components/SiteGrid';
 import {
-  getPhotosCountIncludingHiddenCached,
-  getUniqueTagsCached,
-} from '@/photo/cache';
-import {
-  PATH_ADMIN_PHOTOS,
-  PATH_ADMIN_TAGS,
-  PATH_ADMIN_UPLOADS,
+  PATH_ADMIN_CONFIGURATION,
+  checkPathPrefix,
+  isPathAdminConfiguration,
 } from '@/site/paths';
-import AdminNavClient from './AdminNavClient';
+import { clsx } from 'clsx/lite';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { BiCog } from 'react-icons/bi';
 
-export default async function AdminNav() {
-  const [
-    countPhotos,
-    countUploads,
-    countTags,
-  ] = await Promise.all([
-    getPhotosCountIncludingHiddenCached().catch(() => 0),
-    getStorageUploadUrlsNoStore()
-      .then(urls => urls.length)
-      .catch(e => {
-        console.error(`Error getting blob upload urls: ${e}`);
-        return 0;
-      }),
-    getUniqueTagsCached().then(tags => tags.length).catch(() => 0),
-  ]);
-
-  const navItemPhotos = {
-    label: 'Photos',
-    href: PATH_ADMIN_PHOTOS,
-    count: countPhotos,
-  };
-
-  const navItemUploads = {
-    label: 'Uploads',
-    href: PATH_ADMIN_UPLOADS,
-    count: countUploads,
-  };
-
-  const navItemTags = {
-    label: 'Tags',
-    href: PATH_ADMIN_TAGS,
-    count: countTags,
-  };
-
-  const navItems = [navItemPhotos];
-
-  if (countUploads > 0) { navItems.push(navItemUploads); }
-  if (countTags > 0) { navItems.push(navItemTags); }
+export default function AdminNav({
+  items,
+}: {
+  items: {
+    label: string,
+    href: string,
+    count: number,
+  }[]
+}) {
+  const pathname = usePathname();
 
   return (
-    <AdminNavClient items={navItems} />
+    <SiteGrid
+      contentMain={
+        <div className={clsx(
+          'flex gap-2 md:gap-4',
+          'border-b border-gray-200 dark:border-gray-800 pb-3',
+        )}>
+          <div className={clsx(
+            'flex gap-2 md:gap-4',
+            'flex-grow overflow-x-scroll',
+            '[&::-webkit-scrollbar]:hidden',
+          )}>
+            {items.map(({ label, href, count }) =>
+              <Link
+                key={label}
+                href={href}
+                className={clsx(
+                  'flex gap-0.5',
+                  checkPathPrefix(pathname, href) ? 'font-bold' : 'text-dim',
+                )}
+              >
+                <span>{label}</span>
+                <span>({count})</span>
+              </Link>)}
+          </div>
+          <Link
+            href={PATH_ADMIN_CONFIGURATION}
+            className={isPathAdminConfiguration(pathname)
+              ? 'font-bold'
+              : 'text-dim'}
+          >
+            <BiCog
+              size={18}
+              className="inline-block"
+              aria-label="App Configuration"
+            />
+          </Link>
+        </div>
+      }
+    />
   );
 }
