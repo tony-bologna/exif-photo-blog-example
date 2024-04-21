4c9b710025dde378806d20762ead3bcd47d9e13d
Sam Becker <sam@sambecker.com>
2024-04-20 19:28:33 -0500

    Remove suspense from root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,19 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { clsx } from 'clsx/lite';
 import { IBM_Plex_Mono } from 'next/font/google';
-import { Metadata } from 'next';
 import { BASE_URL, SITE_DESCRIPTION, SITE_TITLE } from '@/site/config';
 import AppStateProvider from '@/state/AppStateProvider';
-import Nav from '@/site/Nav';
 import ToasterWithThemes from '@/toast/ToasterWithThemes';
 import PhotoEscapeHandler from '@/photo/PhotoEscapeHandler';
-import Footer from '@/site/Footer';
-import { Suspense } from 'react';
-import FooterClient from '@/site/FooterClient';
-import NavClient from '@/site/NavClient';
-import CommandK from '@/site/CommandK';
+import { Metadata } from 'next/types';
+import MoreComponentsProvider from '@/state/MoreComponentsProvider';
 import { ThemeProvider } from 'next-themes';
 
 import '../site/globals.css';
 import '../site/sonner.css';
+import NavClient from '@/site/NavClient';
+import CommandKClient from '@/components/CommandKClient';
+import FooterClient from '@/site/FooterClient';
 
 const ibmPlexMono = IBM_Plex_Mono({
   subsets: ['latin'],
@@ -74,28 +72,26 @@ export default function RootLayout({
     >
       <body className={ibmPlexMono.variable}>
         <AppStateProvider>
-          <ThemeProvider attribute="class">
-            <main className={clsx(
-              'mx-3 mb-3',
-              'lg:mx-6 lg:mb-6',
-            )}>
-              <Suspense fallback={<NavClient />}>
-                <Nav />
-              </Suspense>
-              <div className={clsx(
-                'min-h-[16rem] sm:min-h-[30rem]',
-                'mb-12',
+          <MoreComponentsProvider>
+            <ThemeProvider attribute="class">
+              <main className={clsx(
+                'mx-3 mb-3',
+                'lg:mx-6 lg:mb-6',
               )}>
-                {children}
-              </div>
-              <Suspense fallback={<FooterClient />}>
-                <Footer />
-              </Suspense>
-            </main>
-            <CommandK />
-          </ThemeProvider>
+                <NavClient />
+                <div className={clsx(
+                  'min-h-[16rem] sm:min-h-[30rem]',
+                  'mb-12',
+                )}>
+                  {children}
+                </div>
+                <FooterClient />
+              </main>
+              <CommandKClient />
+            </ThemeProvider>
+          </MoreComponentsProvider>
           <Analytics debug={false} />
-          <SpeedInsights debug={false} />
+          <SpeedInsights debug={false}  />
           <PhotoEscapeHandler />
           <ToasterWithThemes />
         </AppStateProvider>
